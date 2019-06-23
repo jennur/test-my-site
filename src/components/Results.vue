952087977a5721be5564da62f57d233b84c58971
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-06-23 21:22:36 +0200

    Add results component

---
+++ src/components/Results.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul v-if="scores" class="results">
+      <li class="results__list-item" v-for="category in Object.keys(scores)" :key="category">
+        <span class="results__score-bar-wrap">
+          <span class="results__score-bar" :style="{width: scores[category]*100 +'%'}"></span>
+        </span>
+        <span>{{category.charAt(0).toUpperCase() + category.slice(1) + " score "}}</span>
+        <b class="results__score">{{scores[category] === null ? "NaN" : scores[category]}}</b>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Results",
+  props: {
+    scores: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  }
+};
+</script>
