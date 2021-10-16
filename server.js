f5a6a2a47542e06f0051258fc83417932f57c635
Jenny Bonsak <jenny.bonsak@gmail.com>
2021-10-16 13:26:22 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const path = './server/app/views/';
+
+const app = express();
+
+app.use(express.static(path));
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
